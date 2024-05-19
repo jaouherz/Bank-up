58658a51aeb7f7e16688333901abf6c7394ec350
mehdi220320 <111698017+mehdi220320@users.noreply.github.com>
2024-05-19 15:52:41 +0100

    mytransaction page

--- projetphp-main/mybankaccount.php
+++ projetphp-main/mybankaccount.php
@@ -28,7 +28,10 @@ if (!$transactions) {
 
 
 
-$stmt = $db->prepare("SELECT * FROM bank_account WHERE id_user = :id");
+$stmt = $db->prepare("SELECT *
+        FROM bank_account
+        JOIN user ON bank_account.id_user = user.User_id
+        WHERE user.User_id = :id");
 $stmt->bindParam(':id', $userid);
 $stmt->execute();
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -58,30 +61,40 @@ include "sidebar.php"
 
     <div class="container">
 
-        <div class="row m-0">
-            <div class="col-md-7 col-12">
-                <div class="row">
+            <div class="col-12 px-0 mb-4" style="width: 87%; margin-left: 9%">
+                <div class="row" >
 
-                    <div class="col-12 mb-4">
+                    <div class="col-12 mb-4" ">
                         <div class="row box-right">
-                            <div class="col-md-8 ps-0 ">
+                            <div class="col-md ps-0">
+                                <h1> <?php echo $user['Lastname'] ?></h1>
+                                <h1> <?php echo $user['Firstname'] ?> </h1>
+                                <h2>Account's id: <b> <?php echo $user['id_account'] ?> </b> </h2>
+                            </div>
+                            <div class="col-md ps-0  " style="    margin-top: 30px;margin-left: 50px;">
                                 <p class="ps-3 textmuted fw-bold h6 mb-0">TOTAL CREDS</p>
                                 <p class="h1 fw-bold d-flex"> <?php
                                     echo $user['solde']
                                     ?> <span class="  textmuted pe-1 h6 align-text-top mt-1"> DT</span> </p>
                                 <p class="ms-3 px-2 bg-green">+10% since last month</p>
-                            </div>
-                            <div class="col-md-4">
+                            </div >
+                            <div class="col-md ps-0">
                                 <p class="p-blue"> <span class="fas fa-circle pe-2"></span>RECIEVED </p>
                                 <p class="fw-bold mb-3"><span class="fas fa-dollar-sign pe-1"></span><?php echo $totalReceived?> <span class="textmuted">.50</span> </p>
                                 <p class="p-org"><span class="fas fa-circle pe-2"></span>SEND</p>
                                 <p class="fw-bold"><span class="fas fa-dollar-sign pe-1"></span><?php echo $totalSent?><span class="textmuted">.00</span></p>
                             </div>
+                            <div class="col-md">
+                                <H1> Make a transaction</H1>
+                                <div class="btn btn-primary  h8" style="display: grid">
+                                    <a href="maketransaction.php" style="color: white;margin-top: -6px;">
+                                        Transaction's Page  <span class="ms-3 fas fa-arrow-right"></span>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-
-                                <div class="col-12 px-0 mb-4">
+                    <div class="col-12 px-0 mb-4">
                                     <section>
                                         <h1>Latest Transactions</h1>
                                         <br>
@@ -157,61 +170,8 @@ include "sidebar.php"
                                         ?>
                                     </section>
                                 </div>
-
-
-
-
-
-
                 </div>
             </div>
-            <div class="col-md-5 col-12 ps-md-5 p-0 ">
-                <div class="box-left">
-                    <p class="textmuted h8">Invoice</p>
-                    <p class="fw-bold h7">Alex Parkinson</p>
-                    <p class="textmuted h8">3897 Hickroy St, salt Lake city</p>
-                    <p class="textmuted h8 mb-2">Utah, United States 84104</p>
-                    <div class="h8">
-                        <div class="row m-0 border mb-3">
-                            <div class="col-6 h8 pe-0 ps-2">
-                                <p class="textmuted py-2">Items</p> <span class="d-block py-2 border-bottom">Legal Advising</span> <span class="d-block py-2">Expert Consulting</span>
-                            </div>
-                            <div class="col-2 text-center p-0">
-                                <p class="textmuted p-2">Qty</p> <span class="d-block p-2 border-bottom">2</span> <span class="d-block p-2">1</span>
-                            </div>
-                            <div class="col-2 p-0 text-center h8 border-end">
-                                <p class="textmuted p-2">Price</p> <span class="d-block border-bottom py-2"><span class="fas fa-dollar-sign"></span>500</span> <span class="d-block py-2 "><span class="fas fa-dollar-sign"></span>400</span>
-                            </div>
-                            <div class="col-2 p-0 text-center">
-                                <p class="textmuted p-2">Total</p> <span class="d-block py-2 border-bottom"><span class="fas fa-dollar-sign"></span>1000</span> <span class="d-block py-2"><span class="fas fa-dollar-sign"></span>400</span>
-                            </div>
-                        </div>
-                        <div class="d-flex h7 mb-2">
-                            <p class="">Total Amount</p>
-                            <p class="ms-auto"><span class="fas fa-dollar-sign"></span>1400</p>
-                        </div>
-                        <div class="h8 mb-5">
-                            <p class="textmuted">Lorem ipsum dolor sit amet elit. Adipisci ea harum sed quaerat tenetur </p>
-                        </div>
-                    </div>
-                    <div class="">
-                        <p class="h7 fw-bold mb-1">Pay this Invoice</p>
-                        <p class="textmuted h8 mb-2">Make payment for this invoice by filling in the details</p>
-                        <div class="form">
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="card border-0"> <input class="form-control ps-5" type="text" placeholder="Card number"> <span class="far fa-credit-card"></span> </div>
-                                </div>
-                                <div class="col-6"> <input class="form-control my-3" type="text" placeholder="MM/YY"> </div>
-                                <div class="col-6"> <input class="form-control my-3" type="text" placeholder="cvv"> </div>
-                                <p class="p-blue h8 fw-bold mb-3">MORE PAYMENT METHODS</p>
-                            </div>
-                            <div class="btn btn-primary d-block h8">PAY <span class="fas fa-dollar-sign ms-2"></span>1400<span class="ms-3 fas fa-arrow-right"></span></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 
